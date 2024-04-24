a1916938cbea70cf7b70a130d7f7303ce13ec5d7
Hayriye <hayriyekartin85@gmail.com>
2024-04-24 22:48:19 +0300

    hayriye-24.04.2024.v11

--- src/test/java/getLandEstate/pages/UnRegisteredPage.java
+++ src/test/java/getLandEstate/pages/UnRegisteredPage.java
@@ -1,5 +1,10 @@
 package getLandEstate.pages;
 
+import getLandEstate.utilities.ui_utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class UnRegisteredPage {
 
 
@@ -19,6 +24,30 @@ public class UnRegisteredPage {
 
     //pınar20
 
+    public UnRegisteredPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "(//a[text()='Contact'])[1]")
+    public WebElement contactMenuButton;
+
+    @FindBy(css = "#firstName")
+    public WebElement firstNameInput;
+
+    @FindBy(css = "#lastName")
+    public WebElement lastNameInput;
+
+    @FindBy(css = "#email")
+    public WebElement emailInput;
+
+    @FindBy(css = "#message")
+    public WebElement messageTextArea;
+
+    @FindBy(xpath = "//div[@class='recaptcha-checkbox-border']")
+    public WebElement notRobotCheckBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement contactMessageSendButton;
 
 
 
