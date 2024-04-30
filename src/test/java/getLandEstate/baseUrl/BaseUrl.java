d762f95b649e70e039cb30b40b17d6038fd14aea
Hayriye <hayriyekartin85@gmail.com>
2024-04-30 23:25:58 +0300

    hayriye-30.04.2024.v15

---
+++ src/test/java/getLandEstate/baseUrl/BaseUrl.java
@@ -0,0 +1,20 @@
+package getLandEstate.baseUrl;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static getLandEstate.utilities.api_utilities.Authentication.token;
+
+public class BaseUrl {
+
+    public static RequestSpecification spec;
+
+    public static void setUp(String email, String password){
+        spec = new RequestSpecBuilder()
+                .setBaseUri("http://www.getlandestate.com:8092")
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Bearer " + token(email, password))
+                .build();
+    }
+}
