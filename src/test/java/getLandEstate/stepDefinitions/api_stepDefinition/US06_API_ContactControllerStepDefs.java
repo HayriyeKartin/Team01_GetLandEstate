a6682326947afa374ee70b9d50bcbf34c3ead53b
Arzu Besiroglu <besirogluarzu@gmail.com>
2024-05-07 18:31:36 +0300

    arzu07.05.2024

---
+++ src/test/java/getLandEstate/stepDefinitions/api_stepDefinition/US06_API_ContactControllerStepDefs.java
@@ -0,0 +1,153 @@
+package getLandEstate.stepDefinitions.api_stepDefinition;
+
+import getLandEstate.pojos.api_pojos.ContactController.ContactMessagesPostPojo;
+import getLandEstate.pojos.api_pojos.ContactController.ContactMessagesGetidResponsePojo;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static getLandEstate.baseUrl.BaseUrl.spec;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+
+
+        /*
+        curl -X 'POST'
+  'http://www.getlandestate.com:8092/contact-messages' \
+
+   -H 'Content-Type: application/json' \
+  -d '{
+        "firstName": "Arzu",
+        "lastName": "Postman",
+        "email": "arzu@postman.com",
+        "message": "API testi için message",
+        "status": true
+        }'
+
+        {
+  "firstName": "string",
+  "lastName": "string",
+  "email": "string",
+  "message": "string",
+  "status": true
+        }
+
+        Resposse body:
+        Contact message saved successfully.
+         */
+
+public class US06_API_ContactControllerStepDefs {
+
+    ContactMessagesPostPojo payload;
+    String postActualData;
+    Response response;
+    static int getId;
+    ContactMessagesGetidResponsePojo actualgetIDResponsePojo;
+    ContactMessagesGetidResponsePojo expectedgetIdPojo;
+    static Map<String,Object> actualDataList;
+    static JsonPath jsonPath;
+
+    @Given("Mesaj olusturmak icin Url duzenlenir")
+    public void mesajOlusturmakIcinUrlDuzenlenir() {
+        spec.pathParam("first","contact-messages");
+    }
+
+    @And("Yeni mesaj icin payload duzenlenir")
+    public void yeniMesajIcinPayloadDuzenlenir() {
+        payload = new ContactMessagesPostPojo("Arzu", "Contact", "arzu@contact.com", "API testi icin message", true);
+    }
+
+    @When("Yeni mesaj icin POST request gonderilir ve Response alinir")
+    public void yeniMesajIcinPOSTRequestGonderilirVeResponseAlinir() {
+        response = given(spec).body(payload).when().post("{first}");
+        postActualData = response.getBody().asString();
+        System.out.println(postActualData);
+    }
+
+    @Then("Yeni mesaj icin Status kodunun {int} oldugu dogrulanir")
+    public void yeniMesajIcinStatusKodununOlduguDogrulanir(int statusCode) {
+        assertEquals(statusCode, response.statusCode());
+    }
+
+    @And("Yeni mesaj icin Response body dogrulanir")
+    public void yeniMesajIcinResponseBodyDogrulanir() {
+        String expectedMessage="Contact message saved successfully.";
+        assertEquals(expectedMessage,postActualData);
+    }
+
+
+    @Given("Mesaji cagirmak icin Url duzenlenir")
+    public void mesajiCagirmakIcinUrlDuzenlenir() {
+        spec.pathParams("first","contact-messages","second",getId);
+
+    }
+
+    @And("Mesaji cagirmak icin GET request gönderilir ve Response alinir")
+    public void mesajiCagirmakIcinGETRequestGönderilirVeResponseAlinir() {
+        response = given(spec).when().get("{first}/{second}");
+        actualgetIDResponsePojo=response.as(ContactMessagesGetidResponsePojo.class);
+        response.prettyPrint();
+
+    }
+
+
+    @Then("Gelen mesaj icin Status kodun {int} oldugu dogrulanir")
+    public void gelenMesajIcinStatusKodunOlduguDogrulanir(int statusCode) {
+        assertEquals(statusCode, response.statusCode());
+    }
+
+
+    @And("Gelen mesajin Response body si dogrulanir")
+    public void gelenMesajinResponseBodySiDogrulanir() {
+        expectedgetIdPojo=new ContactMessagesGetidResponsePojo(getId,"Arzu","Contact","arzu@contact.com","API testi icin message",true,"2024-05-03T22:21:46.582329");
+        assertEquals(expectedgetIdPojo.getId(),actualgetIDResponsePojo.getId());
+        assertEquals(expectedgetIdPojo.getFirstName(),actualgetIDResponsePojo.getFirstName());
+        assertEquals(expectedgetIdPojo.getLastName(),actualgetIDResponsePojo.getLastName());
+        assertEquals(expectedgetIdPojo.getEmail(),actualgetIDResponsePojo.getEmail());
+        assertEquals(expectedgetIdPojo.getMessage(),actualgetIDResponsePojo.getMessage());
+    }
+
+    @Given("Mesajlari cagirmak icin Url duzenlenir")
+    public void mesajlariCagirmakIcinUrlDuzenlenir() {
+        spec.pathParam("first","contact-messages");
+    }
+
+    @Given("Mesajlari cagirmak icin request gönderilir ve Response alinir")
+    public void mesajlariCagirmakIcinRequestGönderilirVeResponseAlinir() {
+        response = given(spec).when().get("{first}");
+        actualDataList =response.as(HashMap.class);
+        jsonPath = response.jsonPath();
+
+    }
+
+    @Then("Gelen mesajlar arasindan bir önceki post isleminde gonderilen mesaj bulunur dogrulanir id si alinir")
+    public void gelenMesajlarArasindanBirÖncekiPostIslemindeGonderilenMesajBulunurDogrulanirIdSiAlinir() {
+        assertEquals("Arzu",jsonPath.getString("content[0].firstName"));
+        assertEquals("Contact",jsonPath.getString("content[0].lastName"));
+        assertEquals("arzu@contact.com",jsonPath.getString("content[0].email"));
+        getId = Integer.parseInt(jsonPath.getString("content[0].id"));
+    }
+
+    @Given("Mesaji silmek icin URL duzenlenir")
+    public void mesajiSilmekIcinURLDuzenlenir() {
+        spec.pathParams("first","contact-messages","second",getId);
+    }
+
+    @And("Mesaji silmek icin DELETE request gonderilir")
+    public void mesajiSilmekIcinDELETERequestGonderilir() {
+        response = given(spec).when().delete("{first}/{second}");
+    }
+
+    @And("Body ile gelen {string} yazisi dogrulanir")
+    public void bodyIleGelenYazisiDogrulanir(String expectedMessage) {
+        assertEquals(expectedMessage,response.getBody().asString());
+    }
+}
+
---
+++ src/test/java/getLandEstate/stepDefinitions/db_stepDefinition/US04_DB_ContactsStepDefs.java
@@ -0,0 +1,88 @@
+package getLandEstate.stepDefinitions.db_stepDefinition;
+
+import getLandEstate.utilities.DB_Utilty;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class US04_DB_ContactsStepDefs {
+    static String sqlQuery;
+    List<String> contactsSutunNames;
+    ResultSet resultSetSutunContactInfo;
+    static String status;
+
+    static String created_at;
+    static int id;
+    static String email;
+    static String first_name;
+    static String last_name;
+    static String message;
+
+
+    @And("Kayitli contacts sutun isimlerini almak icin query gonderilir")
+    public void kayitliContactsSutunIsimleriniAlmakIcinQueryGonderilir() throws SQLException {
+        sqlQuery="select * from contacts";
+        contactsSutunNames= DB_Utilty.getColumnNames(sqlQuery);
+    }
+
+    @Then("Kayitli contacts sutun isimleri dogrulanir")
+    public void kayitliContactsSutunIsimleriDogrulanir() {
+        assertEquals("status",contactsSutunNames.get(0));
+        assertEquals("created_at",contactsSutunNames.get(1));
+        assertEquals("id",contactsSutunNames.get(2));
+        assertEquals("email",contactsSutunNames.get(3));
+        assertEquals("first_name",contactsSutunNames.get(4));
+        assertEquals("last_name",contactsSutunNames.get(5));
+        assertEquals("message",contactsSutunNames.get(6));
+    }
+
+    @And("Kayitli contacts sutun bilgilerini almak icin query gonderilir")
+    public void kayitliContactsSutunBilgileriniAlmakIcinQueryGonderilir() throws SQLException{
+        sqlQuery="select * from contacts where id=351";
+        resultSetSutunContactInfo = DB_Utilty.executeQuery(sqlQuery);
+
+    }
+
+    @Then("Kayitli contacts sutun bilgileri dogrulanir")
+    public void kayitliContactsSutunBilgileriDogrulanir() throws SQLException {
+        status="t";
+        //Tabloda status "true" olarak yazdigi halde, intellij  konsolda testin sonucunu "t" olarak veriyor ve test gecmiyordu
+        //Tabloda dogru oldugu halde format olarak uymasi icin status olarak "Expected :t" olarak kabul edildi
+
+        /*
+        org.junit.ComparisonFailure:
+        Expected :true
+        Actual   :t
+         */
+        created_at="2024-05-05 17:56:37.245773";
+        // Tabloda tarih "5 May 2024 17:56:37" olarak yazdigi halde, intellij konsolda testin sonucunu "2024-05-05 17:56:37.245773" olarak veriyor ve test gecmiyordu
+        // Tarih dogru oldugu halde format olarak uymasi icin tarih olarak "Expected : 2024-05-05 17:56:37.245773" olarak kabul edildi
+        /*
+        org.junit.ComparisonFailure:
+        Expected :5 May 2024 17:56:37
+        Actual   :2024-05-05 17:56:37.245773
+       */
+        id=351;
+        email="arzu@contact.com";
+        first_name="Arzu";
+        last_name="Contact";
+        message="API testi için message2";
+        resultSetSutunContactInfo.next();
+
+        assertEquals(status,resultSetSutunContactInfo.getString("status"));
+        assertEquals(created_at,resultSetSutunContactInfo.getString("created_at"));
+        assertEquals(id,resultSetSutunContactInfo.getInt("id"));
+        assertEquals(email,resultSetSutunContactInfo.getString("email"));
+        assertEquals(first_name,resultSetSutunContactInfo.getString("first_name"));
+        assertEquals(last_name,resultSetSutunContactInfo.getString("last_name"));
+        assertEquals(message,resultSetSutunContactInfo.getString("message"));
+
+
+    }
+}
+
