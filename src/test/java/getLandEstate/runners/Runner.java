276a8a011a887800099fe919f2788e8e47dffb20
Bilal Azun <bilal.azun1903@gmail.com>
2024-05-08 21:05:04 +0300

    Bilal 08.05.2024

--- src/test/java/getLandEstate/runners/Runner.java
+++ src/test/java/getLandEstate/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"getLandEstate/stepDefinitions", "getLandEstate/hooks"},
 
-        tags = "",
+        tags = "@ApiUS_10",
 
         dryRun = false,
         monochrome = false
